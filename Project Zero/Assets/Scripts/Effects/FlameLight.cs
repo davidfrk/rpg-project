91d06f9332f353dcc582fb40ed1f44d2302c7d53
davidfrk <davida1d2@gmail.com>
2019-08-27 07:11:36 -0300

    Unity Update

---
+++ Project Zero/Assets/Scripts/Effects/FlameLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameLight : MonoBehaviour
+{
+    public float amplitude = 0.1f;
+    public float frequency = 1f;
+
+    new Light light;
+    private float baseIntensity;
+
+    void Awake()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void Start()
+    {
+        baseIntensity = light.intensity;
+    }
+
+    void Update()
+    {
+        light.intensity = baseIntensity + amplitude * Mathf.Sin(frequency * Time.time);
+    }
+}
